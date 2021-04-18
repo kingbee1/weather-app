025aa114ef6da87fe9b0ef15682ac201943cb30e
Abiodun <abiodunhodonu@gmail.com>
2021-04-18 23:44:26 +0100

    created a weather app with javascript

---
+++ index.js
@@ -0,0 +1,38 @@
+const apiKey = '6079ed96f9b7c6a8762fa68b3fb79dcd'
+let weather = {
+   fetchWeather: function (city) {
+        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)}`)
+        .then((response) => response.json())
+            .then((data) => {
+                const resData = JSON.parse(data.contents)
+                this.displayWeather(resData)
+            });
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon") .src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search button").addEventListener("click", function () {
+weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+weather.fetchWeather("Lagos");
